00656d0af52f387624007c5101903bde8d8a8571
CikaDraza <drazic.milan@gmail.com>
2024-08-13 10:04:14 +0200

    update db

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,10 +3,12 @@ const data = {
     {
       title: 'Electricons',
       description: 'A great selection of appliances for your home - white goods, small household appliances, televisions, computers, telephones, care appliances, everything you need in one place.',
-      image_url: '/electricons.png',
-      image_logo: 'https://www.electricons.shop/logo/electricons_logo.svg',
+      image_url: 'https://drazic-webdev-server.vercel.app/electricons.png',
+      logo_url: 'https://www.electricons.shop/logo/electricons_logo.svg',
       live_preview_url: 'https://www.electricons.shop/',
       github_url: 'https://github.com/CikaDraza/electricons',
+      color: 'blue',
+      category: 'E-commerce'
     },
   ]
 }
--- src/utils/models/Product.js
+++ src/utils/models/Product.js
@@ -5,9 +5,11 @@ const productSchema = new mongoose.Schema(
     title: {type: String, required: true},
     description: {type: String, required: true},
     image_url: {type: String, required: true},
-    image_logo: {type: String, required: true},
+    logo_url: {type: String, required: true},
     live_preview_url: {type: String, required: true},
     github_url: {type: String, required: true},
+    color: {type: String, required: true},
+    category: {type: String, required: true},
   },
   {
     timestamps: true
