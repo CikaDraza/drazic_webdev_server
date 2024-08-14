2b13394d291f1a1194bf3793db671d3b25561ed7
CikaDraza <drazic.milan@gmail.com>
2024-08-14 07:05:19 +0200

    token

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -2,53 +2,47 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../src/utils/models/User';
 import db from '../src/lib/db';
+import { NextResponse } from 'next/server';
 
-const SECRET_KEY = process.env.JWT_SECRET; // Ensure you have this in your .env file
+const SECRET_KEY = process.env.JWT_SECRET; 
 
 export async function POST(request) {
   try {
-    // Parse request body
+
     const { email, password } = await request.json();
 
-    // Connect to MongoDB
     await db.connect();
 
-    // Find user by email
     const user = await User.findOne({ email });
     if (!user) {
-      return new Response(JSON.stringify({ message: 'User not found' }), {
+      return NextResponse.json({ message: 'User not found' }, {
         status: 404,
         headers: { 'Content-Type': 'application/json' },
       });
     }
 
-    // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return new Response(JSON.stringify({ message: 'Invalid credentials' }), {
+      return NextResponse.json({ message: 'Invalid credentials' }, {
         status: 401,
         headers: { 'Content-Type': 'application/json' },
       });
     }
 
-    // Generate JWT token
     const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, SECRET_KEY, { expiresIn: '1h' });
 
-    // Return success response with token
-    return new Response(JSON.stringify({ token }), {
+    return NextResponse.json({ token }, {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
     console.error('Login error:', error);
 
-    // Return error response
-    return new Response(JSON.stringify({ message: 'Login failed' }), {
+    return NextResponse.json({ message: 'Login failed' }, {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
     });
   } finally {
-    // Disconnect from MongoDB
     await db.disconnect();
   }
 }
