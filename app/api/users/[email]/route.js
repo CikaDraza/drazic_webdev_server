fd7860753bd0c6100ac1c6d1fe85cb833cf95f5a
CikaDraza <drazic.milan@gmail.com>
2024-08-14 08:07:30 +0200

    verify token

--- app/api/users/[email]/route.js
+++ app/api/users/[email]/route.js
@@ -1,10 +1,50 @@
 import db from "@/src/lib/db"; // Import your database connection
 import User from "@/src/utils/models/User"; // Import your User model
 import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+
+const SECRET_KEY = process.env.JWT_SECRET;
 
 export async function GET(request, { params }) {
   try {
     const { email } = params;
+
+    // Extract token from Authorization header
+    const authHeader = request.headers.get('Authorization');
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Unauthorized' }),
+        {
+          status: 401,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          },
+        }
+      );
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    // Verify token
+    try {
+      jwt.verify(token, SECRET_KEY);
+    } catch (error) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Unauthorized' }),
+        {
+          status: 401,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          },
+        }
+      );
+    }
 
     // Connect to MongoDB
     await db.connect();
