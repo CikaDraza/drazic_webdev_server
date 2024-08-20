ade419e76c87e21fcdff98e8aac473ff32876fae
CikaDraza <drazic.milan@gmail.com>
2024-08-20 14:50:50 +0200

    create testimonials/user/[email] route

---
+++ app/api/testimonials/user/[email]/route.js
@@ -0,0 +1,34 @@
+import Testimonial from '@/src/utils/models/Testimonial';
+import User from '@/src/utils/models/User';
+import { NextResponse } from 'next/server';
+
+export async function GET(request, { params }) {
+  const { email } = params;
+
+  try {
+    // Check if the user is an admin
+    const user = await User.findOne({ email });
+    if (!user) {
+      return new NextResponse(
+        JSON.stringify({ message: 'User not found' }),
+        { status: 404 }
+      );
+    }
+
+    // Fetch testimonials based on admin status
+    const testimonials = user.isAdmin
+      ? await Testimonial.find()
+      : await Testimonial.find({ client_email: email });
+
+    return new NextResponse(
+      JSON.stringify(testimonials),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error fetching testimonials:', error);
+    return new NextResponse(
+      JSON.stringify({ message: 'Failed to fetch testimonials' }),
+      { status: 500 }
+    );
+  }
+}
