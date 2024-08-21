739655f7e6432d4622bf521e1709844d4b0ac099
CikaDraza <drazic.milan@gmail.com>
2024-08-21 18:42:41 +0200

    fetch from one route

--- app/api/testimonials/user/[email]/route.js
+++ app/api/testimonials/user/[email]/route.js
@@ -1,4 +1,5 @@
 import Testimonial from '@/src/utils/models/Testimonial';
+import User from '@/src/utils/models/User';
 import { NextResponse } from 'next/server';
 
 export async function OPTIONS(request) {
@@ -19,25 +20,44 @@ export async function GET(request, { params }) {
   const { email } = params;
   const origin = request.headers.get('Origin');
   const allowedOrigins = ['http://localhost:5173', 'https://drazic-webdev.vercel.app'];
-
-  try {        
+  try {
+    console.log(`Fetching testimonials for email: ${email}`);
+    // Check if the user is an admin
+    const user = await User.findOne({ email }).lean();
+    if (!user) {
+      return new NextResponse(
+        JSON.stringify({ message: 'User not found' }),
+        {
+          status: 404,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+        }
+      );
+    }
     
-    const testimonials = await Testimonial.findOne({ client_email: email }).lean();
+    // Fetch testimonials based on admin status    
+    const testimonials = user?.isAdmin
+      ? await Testimonial.find().lean()
+      : await Testimonial.findOne({ client_email: email }).lean()
 
-    return new NextResponse(
-      JSON.stringify(testimonials),
-      {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
-          'Access-Control-Allow-Methods': 'GET, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type',
-        },
-      }
-    );
+      return new NextResponse(
+        JSON.stringify(testimonials),
+        {
+          status: 200,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+        }
+      );
   } catch (error) {
-    console.error('Error fetching testimonials:', error.stack || error.message);
+    console.error('Error fetching testimonials:', error);
     return new NextResponse(
       JSON.stringify({ message: 'Failed to fetch testimonials' }),
       {
