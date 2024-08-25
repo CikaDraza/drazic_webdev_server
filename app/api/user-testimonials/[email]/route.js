658fd56a1a64c5ec69138da022b1c74b351b459a
CikaDraza <drazic.milan@gmail.com>
2024-08-25 23:01:08 +0200

    new route

--- app/api/user-testimonials/[email]/route.js
+++ app/api/user-testimonials/[email]/route.js
@@ -1,28 +1,18 @@
 import Testimonial from '@/src/utils/models/Testimonial';
 import User from '@/src/utils/models/User';
 import { NextResponse } from 'next/server';
-
-export async function OPTIONS(request) {
-  const origin = request.headers.get('Origin');
-  const allowedOrigins = ['http://localhost:5173', 'https://drazic-webdev.vercel.app'];
-  return new NextResponse(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Max-Age': '86400', // Cache the preflight response for 1 day
-    },
-  });
-}
+import db from '@/src/lib/db.js';
 
 export async function GET(request, { params }) {
   const { email } = params;
   const origin = request.headers.get('Origin');
   const allowedOrigins = ['http://localhost:5173', 'https://drazic-webdev.vercel.app'];
+
   try {
     console.log(`Fetching testimonials for email: ${email}`);
-    // Check if the user is an admin
+    
+    await db.connect();
+    
     const user = await User.findOne({ email }).lean();
     if (!user) {
       return new NextResponse(
@@ -39,23 +29,22 @@ export async function GET(request, { params }) {
       );
     }
     
-    // Fetch testimonials based on admin status    
-    const testimonials = user?.isAdmin
+    const testimonials = user.isAdmin
       ? await Testimonial.find().lean()
-      : await Testimonial.findOne({ client_email: email }).lean()
+      : await Testimonial.find({ client_email: email }).lean();
 
-      return new NextResponse(
-        JSON.stringify(testimonials),
-        {
-          status: 200,
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
-            'Access-Control-Allow-Methods': 'GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
-          },
-        }
-      );
+    return new NextResponse(
+      JSON.stringify(testimonials),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
+          'Access-Control-Allow-Methods': 'GET, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      }
+    );
   } catch (error) {
     console.error('Error fetching testimonials:', error);
     return new NextResponse(
@@ -70,5 +59,7 @@ export async function GET(request, { params }) {
         },
       }
     );
+  } finally {
+    await db.disconnect();
   }
 }
