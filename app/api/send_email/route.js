16494aa34fcc96d1283937dbfb650991d8f1b542
CikaDraza <drazic.milan@gmail.com>
2024-10-29 17:40:58 +0100

    new approach

--- app/api/send_email/route.js
+++ app/api/send_email/route.js
@@ -1,108 +1,83 @@
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
-const allowedOrigins = ['http://localhost:5173', 'https://drazic-webdev.dev'];
-
-export async function OPTIONS(request) {
-  const origin = request.headers.get('Origin');
-  return new NextResponse(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
-      'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Max-Age': '86400',
-    },
-  });
-}
-
 export async function POST(request) {
   const origin = request.headers.get('Origin');
-  const { fullName, email, phone, city, text } = await request.body;
+  const allowedOrigins = ['http://localhost:5173', 'https://drazic-webdev.dev'];
+  const data = request.body;
 
   const transporter = nodemailer.createTransport({
-    host: 'mail.privateemail.com',
-    port: 587,
-    secure: false,
+    service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "drazic.milan@gmail.com",
+      pass: "dxko xmrp fqfz tdkd"
     },
-    logger: true,
-    debug: true,
-  });  
+  });
 
   const htmlBody = `
     <html>
       <body>
-        <p>Full Name: ${fullName}</p>
-        <p>Email: ${email}</p>
-        <p>Phone: ${phone}</p>
-        <p>City: ${city}</p>
-        <p>Message: ${text}</p>
+        <p>Ime: ${data.fullName}</p>
+        <p>E-mail: ${data.email}</p>
+        <p>Telefon: ${data.phone}</p>
+        <p>Poruka: ${data.message}</p>
       </body>
     </html>
   `;
 
-  // Email to the user for confirmation
   const userMailOptions = {
-    from: 'contact@drazic-webdev.dev',
-    to: email,
-    subject: 'Thank you for reaching out',
-    text: 'Thank you for contacting us. We have received your message and will get back to you soon.',
+    from: "drazic.milan@gmail.com",
+    to: data.email,
+    subject: 'Hvala vam sto ste nas kontaktirali',
+    text: 'Hvala vam. Vaša poruka je primljena i odgovorićemo vam uskoro.',
     html: htmlBody,
   };
 
-  // Email to the business
-  const businessMailOptions = {
-    from: email,
+  const mailOptions = {
+    from: data.email,
     to: 'contact@drazic-webdev.dev',
-    subject: `New message from ${fullName}`,
-    replyTo: email,
+    subject: `Nova Poruka od ${data.email}`,
+    replyTo: data.email,
     html: htmlBody,
   };
 
   try {
-    // Send confirmation email to the user
     await transporter.sendMail(userMailOptions);
-    console.log('Confirmation email sent to user');
-  } catch (error) {
-    console.error('Error sending email to user:', error);
-  }
-  
-  try {
-    // Send email to the business
-    await transporter.sendMail(businessMailOptions);
-    console.log('Notification email sent to business');
-
+    console.log('E-mail korisniku poslat');
     return new NextResponse(
-      JSON.stringify({ message: 'Message received and emails sent successfully.' }),
-      { 
+      JSON.stringify(userMailOptions),
+      {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
-          'Access-Control-Allow-Methods': 'POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Max-Age': '86400',
-        }
+          'Access-Control-Allow-Methods': 'GET, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
       }
     );
-
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error('Greška prilikom slanja e-maila korisniku:', error);
+  }
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('E-mail poslat');
+    res.status(200).json({ message: 'Podaci su primljeni i obradjeni na serveru. E-mail je poslat.' });
     return new NextResponse(
-      JSON.stringify({ message: 'Error sending email' }),
-      { 
-        status: 500,
+      JSON.stringify(mailOptions),
+      {
+        status: 201,
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : 'null',
           'Access-Control-Allow-Methods': 'POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Max-Age': '86400',
-        }
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
       }
     );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Došlo je do greške prilikom slanja e-maila.' });
   }
-}
+};
